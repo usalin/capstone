d341b2f7b1464b3a3a50328a8eca45184ae1b3bc
ugur salin <ugursalin@gmail.com>
2022-03-12 11:37:52 +0300

    added the logout functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [Auth0Guard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
