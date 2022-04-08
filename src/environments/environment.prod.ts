b523e35c6dca8f0362a93addd90d269f7d4db0b7
ugur salin <ugursalin@gmail.com>
2022-04-08 13:30:40 +0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://music-palace.herokuapp.com',
+  localServerUrl: 'http://localhost:3000',
+  firebase: {
+    apiKey: "AIzaSyAoGIO0wrjHu0l3vuXOSYFYcz54csK3RtU",
+    authDomain: "ecommerceaudio.firebaseapp.com",
+    projectId: "ecommerceaudio",
+    storageBucket: "ecommerceaudio.appspot.com",
+    messagingSenderId: "537771781365",
+    appId: "1:537771781365:web:d93e77c1da9e3d664de9bc"
+  }
 };
